27933be78dd3a1d706ee833a274087b8ea6f7106
solliolli <olli.herrala@aalto.fi>
2023-09-13 13:49:21 +0300

    Small improvements

--- code/.ipynb_checkpoints/pi_montecarlo-checkpoint.jl
+++ code/.ipynb_checkpoints/pi_montecarlo-checkpoint.jl
@@ -3,8 +3,8 @@ N_sample = 100000
 distances = Array{Float64}(undef, N_sample)
 pi_estimate = Array{Float64}(undef, N_sample)
 sample = rand(2, N_sample)
+distances = sqrt.(sample[1,:].^2 + sample[2,:].^2)
 for i in 1:N_sample
-    distances[i] = sqrt.(sample[1,i].^2 + sample[2,i].^2)
     pi_estimate[i] = 4*count(dist -> dist <= 1, distances[1:i])/i
 end
 println("The final estimate of the value of pi is $(pi_estimate[end])")
--- code/slurmjob_pi.jl
+++ code/slurmjob_pi.jl
@@ -5,8 +5,8 @@ N_sample = 100000
 distances = Array{Float64}(undef, N_sample)
 pi_estimate = Array{Float64}(undef, N_sample)
 sample = rand(2, N_sample)
+distances = sqrt.(sample[1,:].^2 + sample[2,:].^2)
 for i in 1:N_sample
-    distances[i] = sqrt.(sample[1,i].^2 + sample[2,i].^2)
     pi_estimate[i] = 4*count(dist -> dist <= 1, distances[1:i])/i
 end
 writedlm("results/array_$(idx).csv", pi_estimate)
